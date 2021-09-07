97af7fa7cff9b7152f2859c3df69d8e7cfd8ec17
Sergey Levchenko <sergey@levchenko.pw>
2021-09-07 21:35:29 +0300

    add solution for 1-module 1-task

--- 01-intro/01-sum/sum.js
+++ 01-intro/01-sum/sum.js
@@ -1,5 +1,21 @@
-function sum(a, b) {
-  /* ваш код */
-}
+// Немного усложнил себе задачу, аргументов может быть больше двух
+const sum = function(a, b) {
+  // eslint-disable-next-line prefer-rest-params
+  const arr = [...arguments];
+
+  if (arr.some((value) => typeof value !== 'number')) {
+    throw new TypeError('Один или несколько аргументов не являются числами');
+  }
+
+  // решение(в лоб) стажера с двумя аргументами
+  // return a + b;
+
+  // классика
+  // return arr.reduce((sum, cur) => sum + cur);
+
+  // Выпендрился :)
+  // Оригинальное решение :) так обычно быстрее всего сложить числа из массива
+  return eval(arr.join('+'));
+};
 
 module.exports = sum;
