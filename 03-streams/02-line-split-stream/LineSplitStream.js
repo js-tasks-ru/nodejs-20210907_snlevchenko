464a203688c19e5ed106417e3b4a1ffac546841a
Sergey Levchenko <sergey@levchenko.pw>
2021-09-28 19:27:38 +0300

    add solution for 3-module 2-task

--- 03-streams/02-line-split-stream/LineSplitStream.js
+++ 03-streams/02-line-split-stream/LineSplitStream.js
@@ -4,12 +4,35 @@ const os = require('os');
 class LineSplitStream extends stream.Transform {
   constructor(options) {
     super(options);
+    this.str = '';
   }
 
   _transform(chunk, encoding, callback) {
+
+    this.str += chunk;
+
+    if (chunk.includes(os.EOL)) {
+      const chunks = this.str.toString().split(os.EOL);
+      if (chunks.length === 1) {
+        this.push(chunk);
+        this.str = '';
+      } else {
+        chunks.forEach((subchunk, idx) => {
+          if (idx !== chunks.length - 1) {
+            this.push(subchunk);
+          } else {
+            this.str = subchunk;
+          }
+        });
+      }
+    }
+
+    callback();
   }
 
   _flush(callback) {
+    this.push(this.str);
+    callback();
   }
 }
 
