081158e87834534ad32837965374db5126345c02
Sergey Levchenko <sergey@levchenko.pw>
2021-09-28 19:45:33 +0300

    add solution for 3-module 3-task

--- 03-streams/03-file-server-get/server.js
+++ 03-streams/03-file-server-get/server.js
@@ -1,8 +1,10 @@
-const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
 const server = new http.Server();
+
+const isFirstLevelPath = (pathname) => pathname.split('/').length === 1;
 
 server.on('request', (req, res) => {
   const url = new URL(req.url, `http://${req.headers.host}`);
@@ -12,7 +14,23 @@ server.on('request', (req, res) => {
 
   switch (req.method) {
     case 'GET':
-
+      const stream = fs.createReadStream(filepath);
+      stream.pipe(res);
+      stream.on('error', (err) => {
+        if (err.code === 'ENOENT') {
+          if (!isFirstLevelPath(pathname)) {
+            res.statusCode = 400;
+            res.end('Bad request');
+          } else {
+            res.statusCode = 404;
+            res.end('Not Found');
+          }
+        } else {
+          res.statusCode = 500;
+          res.end('Server error');
+        }
+      });
+      req.on('aborted', () => stream.destroy());
       break;
 
     default:
